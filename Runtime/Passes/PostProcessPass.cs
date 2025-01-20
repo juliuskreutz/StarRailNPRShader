0520fc219d7bffcb384bcd01b262aac6fd871c0f
Julius Kreutz <julius@kreutz.dev>
2025-01-20 15:43:06 +0100

    Update PostProcessPass.cs

--- Runtime/Passes/PostProcessPass.cs
+++ Runtime/Passes/PostProcessPass.cs
@@ -294,7 +294,7 @@ namespace HSR.NPRShader.Passes
                     colorTargetHandle = m_BloomCharacterColor;
                 }
 
-                CoreUtils.SetKeyword(material, ShaderKeywordStrings.UseRGBM, m_UseRGBM);
+                // CoreUtils.SetKeyword(material, ShaderKeywordStrings.UseRGBM, m_UseRGBM);
 
                 cmd.SetGlobalFloat(PropertyIds._BloomThreshold, m_BloomConfig.Threshold.value);
                 cmd.SetGlobalVectorArray(PropertyIds._BloomUVMinMax, m_BloomAtlasUVMinMax);
